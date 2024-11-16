d580011b5b85591a22ceb2c420c66231aa6d6509
Dark-Flames1 <56840412+Dark-Flames1@users.noreply.github.com>
2024-11-16 13:25:07 -0700

    Update Customer.java
    rough draft based on provided diagrams

--- src/Customer.java
+++ src/Customer.java
@@ -5,4 +5,29 @@ public class Customer {
     private int age;
     private String contactInfo;
     private Team team;
+
+    //construct
+    public Customer(int playerID, String name, int age, String contactInfo){
+        this.playerID = playerID;
+        this.name = name;
+        this.age = age;
+        this.contactInfo = contactInfo;
+    }
+
+    //getters
+    public int getPlayerID(){
+        return playerID;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public String getContactInfo(){
+        return contactInfo;
+    }
 }
