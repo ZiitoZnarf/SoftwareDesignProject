804566af7a6a900df769087c686211ff34237dee
Dark-Flames1 <56840412+Dark-Flames1@users.noreply.github.com>
2024-11-16 13:29:31 -0700

    Update Room.java
    rough draft based on diagrams

--- src/Room.java
+++ src/Room.java
@@ -10,5 +10,29 @@ public class Room {
     private Leaderboard leaderboard;
     private List<Feedback> feedback;
 
+    //construct
+    public Room(int roomID, int difficultyLevel, int maxParticipant, String status, String theme){
+        this.roomID = roomID;
+        this.difficultyLevel = difficultyLevel;
+        this.maxParticipant = maxParticipant;
+        this.status = status;
+        this.theme = theme;
+    }
 
+    //getter setter
+    public int getRoomID(){
+        return roomID;
+    }
+
+    public int getMaxParticipant(){
+        return maxParticipant;
+    }
+
+    public String getStatus(){
+        return status;
+    }
+
+    public void setStatus(String status){
+        this.status = status;
+    }
 }
