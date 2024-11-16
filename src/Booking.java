a227b11281e585b294c8593490327d7468354477
Dark-Flames1 <56840412+Dark-Flames1@users.noreply.github.com>
2024-11-16 13:32:49 -0700

    Update Booking.java
    rough draft based on diagrams

--- src/Booking.java
+++ src/Booking.java
@@ -6,4 +6,48 @@ public class Booking {
     private String confirmation;
     private Room room;
     private Customer customer;
-}
+
+    //construct
+    public Booking(int bookingId, float downPayment, int numberOfParticipants, String timeSlot, Room room, Customer customer){
+        this.bookingId = bookingId;
+        this.downPayment = downPayment;
+        this.numberOfParticipants = numberOfParticipants;
+        this.timeSlot = timeSlot;
+        this.room = room;
+        this.customer = customer;
+        this.confirmation = "Pending"; 
+    }
+
+    //getter setter
+    public int getBookingId(){
+        return bookingId;
+    }
+
+    public void setConfirmation(String confirmation){
+        this.confirmation = confirmation;
+    }
+
+    public String getConfirmation(){
+        return confirmation;
+    }
+
+    public Room getRoom(){
+        return room;
+    }
+
+    public Customer getCustomer(){
+        return customer;
+    }
+
+    public int getNumberOfParticipants(){
+        return numberOfParticipants;
+    }
+
+    public String getTimeSlot(){
+        return timeSlot;
+    }
+
+    public float getDownPayment(){
+        return downPayment;
+    }
+}}
