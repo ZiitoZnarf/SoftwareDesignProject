2b981480e8d7ccd0f557f5a5be1b8ac5b4ee589e
Christian S. <126751084+ZiitoZnarf@users.noreply.github.com>
2024-12-06 16:26:09 -0700

    Moved builder statements around to match object communication diagram.

--- src/ManageBooking.java
+++ src/ManageBooking.java
@@ -17,40 +17,50 @@ public class ManageBooking {
     //reserve room
     public String reserveRoom(int customerID, String timeSlot, int roomID, int numParticipants){
         //check if cust exist
+        bookingBuilder.reset();
+
         Customer customer = manageAccount.getCustomerByID(customerID);
         if (customer == null) {
             return "Customer not found. Please register first.";
         }
+        bookingBuilder.setCustomer(customer);
 
         //payment info check
         if (customer.getPaymentInfo() == null) {
             return "Customer payment info is missing. Please update your payment information.";
         }
 
+
         //check room avail
         if (!manageRoom.isRoomAvailable(roomID, timeSlot, bookings)) {
             return "Room is not available at the specified time slot.";
         }
+        bookingBuilder.setTimeSlot(timeSlot);
+
 
         //check room exist / player count
         Room room = manageRoom.getRoomByID(roomID);
         if (room == null) {
             return "Room not found.";
         }
+        bookingBuilder.setRoom(room);
+
         if (numParticipants > room.getMaxParticipant()) {
             return "Number of participants exceeds room capacity.";
         }
+        bookingBuilder.setNumberOfParticipants(numParticipants);
 
         //reserve
         int bookingID = bookings.size() + 1;
+        bookingBuilder.setBookingId(bookingID);
+
         float downPayment = 0.2f * numParticipants * room.getBasePrice();
-        //Booking newBooking = new Booking(bookingID, downPayment, numParticipants, timeSlot, room, customer);
-        Booking newBooking = this.bookingBuilder.reset().setBookingId(bookingID).setDownPayment(downPayment).
-                setCustomer(customer).setNumberOfParticipants(numParticipants).setTimeSlot(timeSlot).setRoom(room).build();
-        bookings.add(newBooking);
+        bookingBuilder.setDownPayment(downPayment);
+
+        bookings.add(bookingBuilder.build());
 
         //update status
-        room.setStatus("booked");
+        //room.setStatus("booked");
         return "Room reserved successfully.";
     }
 }
